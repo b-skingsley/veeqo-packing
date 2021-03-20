3cd8275a5dc295b4d2ff6b28672343cce4915c75
ben <benjaminskingsley@gmail.com>
2021-03-20 12:01:51 +0000

    Added Pseudo code in app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  # Check volume of all items in order
+
+  # Compare volume with available box to discount if box won't fit items
+
+  # If it fits with volume check that it can take biggest item
+
+  # If it can take biggest item check what dimensions are left over
+
+  # Check dimensions for next biggest item
+
+  # Keep going until no items left
 end
