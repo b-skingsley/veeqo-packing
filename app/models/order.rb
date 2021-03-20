15c8562561d8954cca8ac6cd39c50f6720637738
ben <benjaminskingsley@gmail.com>
2021-03-20 18:43:59 +0000

    Added has many and belongs to

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,3 @@
 class Order < ApplicationRecord
-  attr_reader :id, :customer_name, :items
-
-  def initialize(props)
-    @id = props[:id]
-    @customer_name = props[:customer_name]
-    @items = props[:items]
-  end
+  has_many :order_items
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,20 +1,3 @@
 class OrderItem < ApplicationRecord
-  attr_reader :title, :image_url, :sku, :weight, :width, :height, :depth, :dimensions
-
-  def initialize(props)
-    @title = props[:title]
-    @image_url = props[:image_url]
-    @sku = props[:sku_code]
-    @weight = props[:weight]
-    @width = props[:width]
-    @height = props[:height]
-    @depth = props[:depth]
-    @dimensions = calc_dimensions
-  end
-
-  private
-
-  def calc_dimensions
-    [@width, @height, @depth]
-  end
+  belongs_to :order
 end
